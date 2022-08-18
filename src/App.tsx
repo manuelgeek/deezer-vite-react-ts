8d925afff3c03b90daa6e30d244d05a024863959
Emmanuel Magak <emashmagak@gmail.com>
2022-08-18 22:27:54 +0300

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+          
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
